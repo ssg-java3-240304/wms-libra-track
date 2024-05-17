24d0ac4b4350af12fcf18bf71842aa2b2d0e94ff
heem6woo <heem6woo@gmail.com>
2024-05-17 18:21:57 +0900

    Feat: in_warehousing command testing sql

---
+++ src/main/resources/sql/inwarehousing/commandTest.sql
@@ -0,0 +1,8 @@
+﻿insert into in_warehousing (date, status, publisher_manager_id) values
+('2021-06-01', 'pending', 1);
+
+INSERT INTO member values (1, '우희민', 'admin', 'admin', 'admin', '12345678910', 'dddd@gmail.com')
+
+INSERT INTO publisher (name, email, phone_number, business_number) value ('흼출판사', 'admin@heem.com','01012345678', '1234567891')
+
+INSERT INTO publishermanager (member_id, publisher_id) value (1, 1)
