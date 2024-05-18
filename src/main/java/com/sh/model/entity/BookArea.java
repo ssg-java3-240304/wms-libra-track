92f596a82b782155631ce8ece50d8a9ec81763a6
heem6woo <heem6woo@gmail.com>
2024-05-18 13:16:55 +0900

    Feat: 재고 구역 entity 추가

---
+++ src/main/java/com/sh/model/entity/BookArea.java
@@ -0,0 +1,9 @@
+package com.sh.model.entity;
+
+public class BookArea {
+    private int bookAreaId;
+    private int areaId;
+    private int bookId;
+    private int quantity;
+    private int reserved;
+}
