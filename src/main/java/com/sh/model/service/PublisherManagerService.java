f4ba280d495d1de69a5e0dac12100ebda1b2e63f
Hong-ji-min <jmh907@naver.com>
2024-05-20 18:53:11 +0900

    Feat : 출판사 매니저 회원 정보 조회 기능

--- src/main/java/com/sh/model/service/PublisherManagerService.java
+++ src/main/java/com/sh/model/service/PublisherManagerService.java
@@ -1,12 +1,6 @@
 package com.sh.model.service;
 
 
-import com.sh.common.MyBatisTemplate;
-import com.sh.model.dao.InWarehousingDao;
-import com.sh.model.dao.InventoryManagerMapper;
-import com.sh.model.dao.PublisherManagerMapper;
-import com.sh.model.entity.InventoryManagerDto;
-import com.sh.model.entity.MemberDto;
 import com.sh.model.dao.PublisherManagerMapper;
 import com.sh.model.entity.PublisherManagerDto;
 import org.apache.ibatis.session.SqlSession;
@@ -16,28 +10,22 @@ import static com.sh.common.MyBatisTemplate.getSqlSession;
 //홍지민 작업 시작
 public class PublisherManagerService {
     // 출판사 매니저 정보 조회
-    public PublisherManagerDto findPublisherManager(String id) {
-        SqlSession sqlSession = getSqlSession();
-        PublisherManagerMapper publisherManagerMapper = sqlSession.getMapper(PublisherManagerMapper.class);
-        try {
-            PublisherManagerDto result = publisherManagerMapper.findPublisherManager(id);
-            sqlSession.commit();
-
-            return result;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        } finally {
-            sqlSession.close();
-        }
-
-
-    }
-
-    public MemberDto findPublisherManagerInfo(int memberId) {
+    public PublisherManagerDto findPublisherManagerInfo(int memberId) {
         try (SqlSession sqlSession = getSqlSession()) {
             PublisherManagerMapper publisherManagerMapper = sqlSession.getMapper(PublisherManagerMapper.class);
-            MemberDto memberDto = publisherManagerMapper.findPublisherManagerInfo(memberId);
-            return memberDto;
+            PublisherManagerDto publisherManagerDto = publisherManagerMapper.findPmInfo(memberId);
+            return publisherManagerDto;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // 출판사 매니저 정보 수정
+    public PublisherManagerDto updatePublisherManager(int memberId) {
+        try (SqlSession sqlSession = getSqlSession()) {
+            PublisherManagerMapper publisherManagerMapper = sqlSession.getMapper(PublisherManagerMapper.class);
+            PublisherManagerDto publisherManagerDto = publisherManagerMapper.updatePmInfo(memberId);
+            return publisherManagerDto;
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
